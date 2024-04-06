b0d9937a35f6b88ccebb4ca27224b3ac7d8ad4a9
Kairos-T <hestiatay@gmail.com>
2024-04-07 06:49:59 +0800

    feat: added onload text messages + animations

--- app/static/script.js
+++ app/static/script.js
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // Chat
 
 document.addEventListener("DOMContentLoaded", () => {
+    onLoad()
     const inputField = document.getElementById("messageInput");
     inputField.addEventListener("keydown", (e) => {
         if (e.code === "Enter" && inputField.value) {
@@ -30,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// TODO: Add
+// TODO: Add response logic
 function output(input) {
     let response;
 
@@ -41,13 +42,18 @@ function output(input) {
 
 chatLog = [];
 
+function onLoad() {
+    addChat("Hello!", "Welcome to WTS\' chatbot! How can I help you?");
+
+}
+
 function addChat(input, response) {
     const messagesContainer = document.getElementById("messages");
 
     let userDiv = document.createElement("div");
     userDiv.id = "user";
     userDiv.innerHTML = `
-    <article class="message m-5  is-primary" >
+    <article class="message m-5 is-primary" style="animation: fadeIn 2s;" >
         <div class="message-body" style="display: flex; align-items: center; background-color: lightgray">
             <img src="https://cdn-icons-png.freepik.com/512/64/64572.png?ga=GA1.1.334113719.1712415648&" class="avatar" alt="User's profile picture" style="width: 30px; height: 30px; margin-right: 10px;">
             <span class="subtitle is-6" style="flex: 1;"><strong>You</strong><br>${input}</span>
@@ -59,18 +65,22 @@ function addChat(input, response) {
     let botDiv = document.createElement("div");
     botDiv.id = "bot";
 
-    botDiv.innerHTML = `
-    <article class="message m-5  is-link" >
-        <div class="message-body" style="display: flex; align-items: center; background-color: lightgray">
-            <img src="../static/assets/chatbot.png" class="avatar" alt="Bot's profile picture" style="width: 30px; height: 30px; margin-right: 10px; border-radius: 50%">
-            <span class="subtitle is-6" style="flex: 1;"><strong>Thomas</strong><br>${response}</span>
-        </div>
-    </article>
-`;
+    setTimeout(() => {
 
-    messagesContainer.appendChild(botDiv);
-    chatLog.push({ user: input, bot: response });
+        botDiv.innerHTML = `
+        <article class="message m-5  is-link"  style="animation: fadeIn 2s;">
+            <div class="message-body" style="display: flex; align-items: center; background-color: lightgray">
+                <img src="../static/assets/chatbot.png" class="avatar" alt="Bot's profile picture" style="width: 30px; height: 30px; margin-right: 10px; border-radius: 50%">
+                <span class="subtitle is-6" style="flex: 1;"><strong>Thomas</strong><br>${response}</span>
+            </div>
+        </article>
+    `;
+        messagesContainer.appendChild(botDiv);
+    }, 1000);
+
+    chatLog.push({user: input, bot: response});
     console.log(chatLog)
 
     messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
 }
+
