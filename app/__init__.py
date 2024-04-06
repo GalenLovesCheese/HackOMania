2aba0c522b04755ed8437fd188e40462e1fe0c5d
Sam <samuelkoh17@gmail.com>
2024-04-06 16:21:27 +0800

    yay managed to make a basic mongo db

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from pymongo import MongoClient
 
 def create_app():
     # Init app
@@ -9,3 +10,12 @@ def create_app():
     app.register_blueprint(main_bp, url_prefix="/")
 
     return app
+
+app = Flask(__name__)
+
+#27017 is Port no. of host 'localhost'
+client = MongoClient('localhost', 27017) 
+
+db = client.flask_db
+todos = db.todos
+
